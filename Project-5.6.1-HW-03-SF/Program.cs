d6a53a3deba22a6e54b5161527c99349cd6b214f
krukd <krukd87@gmail.com>
2023-04-21 22:03:56 +0200

    Добавьте файлы проекта.

---
+++ Project-5.6.1-HW-03-SF/Program.cs
@@ -0,0 +1,10 @@
+﻿namespace Project_5._6._1_HW_03_SF
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
